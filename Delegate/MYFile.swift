581da7e9ebc7590985b031eb366cb87f5e9c6138
Subash Luitel <luitel@me.com>
2018-01-12 13:06:33 -0600

    lkgjdas

---
+++ Delegate/MYFile.swift
@@ -0,0 +1,15 @@
+//
+//  MYFile.swift
+//  SLStart
+//
+//  Created by Subash Luitel on 1/12/18.
+//
+
+import Foundation
+import UIKit
+
+public class MYLogin {
+    public func whatIsIt() {
+        print("This is print")
+    }
+}
